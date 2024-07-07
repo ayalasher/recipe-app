4befa2de2fb196c760e02b86063e157bca250670
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-07-07 22:16:19 +0300

    The display ingredients bug was fixed

--- recipe-app/src/components/Itemlist.jsx
+++ recipe-app/src/components/Itemlist.jsx
@@ -5,15 +5,22 @@
 
 // setloading(true)
 
-export default function Itemlist(food,loading) {
-    return <div>
-                {loading? <p>ingredients loading ...</p>:food.extendedIngredients.map((item)=> <div>
-
-<img src={`https://spoonacular.com/cdn/ingredients_70*70`+item.image} alt="" />
-
-<h5> {item.name} </h5>
-<h5> {item.amount}  {item.unit}   </h5>
-</div>) }
-        
-    </div>
-}
+export default function Itemlist({ food, loading }) {
+    return (
+        <div>
+            {loading ? (
+                <p>Loading ingredients</p>
+            ) : (
+                food.extendedIngredients.map((item) => (
+                    <div key={item.id}>
+                        <img src={`https://spoonacular.com/cdn/ingredients_70x70/${item.image}`} alt={item.name} />
+                        <h5>{item.name}</h5>
+                        <h5>
+                            {item.amount} {item.unit}
+                        </h5>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+}   
